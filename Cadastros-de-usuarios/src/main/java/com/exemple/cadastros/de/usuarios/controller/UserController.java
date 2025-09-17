04bbe6df0ae211a9dd55d113a24ad2de35948627
gab.dev <cadetenascimento5@gmail.com>
2025-09-17 10:11:12 -0300

    atualização do commit

--- Cadastros-de-usuarios/src/main/java/com/exemple/cadastros/de/usuarios/controller/UserController.java
+++ Cadastros-de-usuarios/src/main/java/com/exemple/cadastros/de/usuarios/controller/UserController.java
@@ -12,9 +12,7 @@ import java.util.List;
 
 public class UserController {
 
-    //injeção de dependência
     private final UserService userService;
-    //
 
     public UserController(UserService userService) {
         this.userService = userService;
